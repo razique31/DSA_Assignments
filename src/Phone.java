e0618c20b5086fa6d8fb63a6f01497ac53ead9f9
raz <mdrazique31@gmail.com>
2024-03-21 22:51:20 +0530

    initial

---
+++ src/Phone.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Phone{
+    String area_code,exchange,number;
+    Phone(String area_code, String exchange, String number){
+        this.area_code = area_code;
+        this.exchange = exchange;
+        this.number  = number;
+    }
+
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the area code: ");
+        this.area_code = sc.nextLine();
+        System.out.print("Enter the exchange: ");
+        this.exchange = sc.nextLine();
+        System.out.print("Enter the number: ");
+        this.number = sc.nextLine();
+    }
+    void display(){
+        System.out.printf("(%s) %s-%s",this.area_code,this.exchange,this.number);
+    }
+}
+     class PhoneDriver{
+    public static void main(String[] args){
+        Phone number1 = new Phone("212","767","8900");
+        Phone number2 = new Phone("","","");
+        number2.input();
+        System.out.print("My number is ");
+        number1.display();
+        System.out.print("\nYour number is ");
+        number2.display();
+    }
+}
